ad2b26a11c3d2a55dfb5dde0ae3734013ec5525d
Mike Leavy <mleavy@gmail.com>
2020-10-21 14:04:45 -0700

    SPM manifest

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "ScrollableGraphView",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "ScrollableGraphView", targets: ["ScrollableGraphView"])
+    ],
+    targets: [
+        .target(name: "ScrollableGraphView", path: "Sources")
+    ]
+)
